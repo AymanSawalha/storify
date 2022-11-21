ce24a87a6f302cdc7b1f9de4c9e59b45dd2a5a68
Mohammad Sawalha <117194779+mohammad-e-sawalha@users.noreply.github.com>
2022-11-21 12:58:15 +0200

    Revert "AddEditFunc"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import AddCompany from './components/Addcompany';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import AddCompany from './components/AddCompany';
+import AddTableRow from './components/AddTableRow';
+import TableHead from './components/TableHead';
 function App() {
-    return (  
-   <AddCompany />
-    )
+
+  return (
+    <div id="card">
+      <div className="row"id="card">
+        <div className="col-lg-6 mb-4"id="card">
+          <div className="card shadow mb-4"id="card">
+            <div className="card-header py-3"id="card">
+              <h6 className="m-0 font-weight-bold ">Companies</h6>
+              <AddCompany/>
+            </div>
+            <div className="card-body"id="card">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default App;
